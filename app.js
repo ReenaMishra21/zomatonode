c49f88dc99750d1ab8a0c64d14b27246d3708e50
ReenaMishra21 <rmshra.smg@gmail.com>
2021-12-01 16:19:37 +0530

    port no.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ dotenv.config();
 var mongoUrl = 'mongodb+srv://dbase1:pswd@cluster0.1uetv.mongodb.net/augintern?retryWrites=true&w=majority';
 var cors = require('cors')
 const bodyParser = require('body-parser')
-var port = process.env.PORT || 8124;
+var port = process.env.PORT || 8125;
 // save the database connection
 var db;
 
